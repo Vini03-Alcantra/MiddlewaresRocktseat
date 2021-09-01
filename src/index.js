9fc1a307c52cb4ca3fa3c258d2b29d4c0e1cd50e
Vinicius de Alcantra Barros <viniciusalcantarabarros@gmail.com>
2021-09-01 13:10:19 -0300

    Desafio 2 da Rocktseat trabalhando com middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((user) => user.username === username)
+  if(!user){
+    return response.status(404).json({error: "User not exists"})
+  }
+
+  request.user = user;
+  next()
+  
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+
+  const usersCounted = user.todos.length;
+
+  if(!(user.pro) && usersCounted > 9){
+    return response.status(403).json({error: "User not exists"})
+  }
+
+  request.user = user
+  next()
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers; 
+  const {id} = request.params;
+
+  if(!validate(id)){
+    return response.status(400).json({msg: "ID invalid"})
+  }
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({msg: "user don't exists"})
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({msg: "ID don't exists"})
+  }
+
+
+  request.todo = todo;
+  request.user = user;
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find((user) => user.id ===  id)
+
+  if(!user){
+    return response.status(404).json({error: "ID invalid"})
+  }
+
+  request.user = user;
+  next()
 }
 
 app.post('/users', (request, response) => {
